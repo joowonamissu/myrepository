d57548a6aa5fcd9440c7d6c041891ed17ebfa770
Alex <joowona@naver.com>
2021-01-22 18:38:18 +0900

    introduce R

---
+++ datasets.R
@@ -0,0 +1,21 @@
+search()
+help(package = datasets)
+library(help = datasets)
+iris
+data("iris")
+airquality
+data("airquality")
+mtcars
+data("mtcars")
+help("airquality")
+ install.packages('ggplot2')
+library(ggplot2) 
+help(package = "ggplot2") 
+library(help = "ggplot2")
+data(package = 'ggplot2')
+data(mpg,package = 'ggplot2')
+mpg
+help(mpg)
+install.packages('readr') 
+library(readr)
+guess_encoding('customer.txt')
